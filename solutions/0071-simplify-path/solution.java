485fa6b93f8e781b148b6363a237da15fee8688d
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-07-23 15:44:52 +0000

    [LeetCode Sync] Runtime - 8 ms (9.82%), Memory - 45.1 MB (37.74%)

---
+++ solutions/0071-simplify-path/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public static String simplifyPath(String path) {
+        String[] str = path.split("/");
+        Stack<String> s = new Stack<>();
+        for(int i=0;i<str.length;i++){
+            String curr = str[i];
+            if(curr.equals(".") || curr.equals("")){
+                continue;
+            }
+            else if(curr.equals("..")){
+                if(!s.isEmpty()){
+                    s.pop();
+                }
+            }
+            else{
+                s.push(curr);
+            }
+        }
+        String result = "";
+        while(!s.isEmpty()){
+            result = "/" + s.pop() + result;
+        }
+        if (result.equals("")) {
+    return "/";
+}
+
+
+        return result;
+    }
+    public static void main (String [] args){
+        String path = "/home/";
+        System.out.println(simplifyPath(path));
+    }
+}
