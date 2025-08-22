4e08247eb56c22aa1d4b11c383a928bba71a15d2
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-08-22 10:21:25 +0000

    [LeetCode Sync] Runtime - 1 ms (87.07%), Memory - 44.3 MB (99.93%)

---
+++ solutions/0046-permutations/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public static void backtrack(List<List<Integer>> resultList, ArrayList<Integer> ans, int[] nums) {
+        // If we match the length, it is a permutation
+        if (ans.size() == nums.length) {
+            resultList.add(new ArrayList<>(ans));
+            return;
+        }
+        for (int num : nums) {
+            if (ans.contains(num)) {
+                continue;
+            }
+            ans.add(num);
+            backtrack(resultList, ans, nums);
+            ans.remove(ans.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtrack(resultList,new ArrayList<>(),nums);
+        return resultList;
+    }
+}
