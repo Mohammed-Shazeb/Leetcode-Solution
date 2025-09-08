9371e134886145127d16a3832a3eedded6cd296e
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-09-08 16:40:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (100.00%)

---
+++ solutions/2102-find-the-middle-index-in-array/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+         int[] leftPreSum = new int[nums.length];
+        int[] rightPreSum = new int[nums.length];
+        int n = nums.length;
+
+        leftPreSum[0] = nums[0];
+        rightPreSum[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            leftPreSum[i] = leftPreSum[i - 1] + nums[i];
+            int j = n - i - 1;
+            if (j >= 0) rightPreSum[j] = rightPreSum[j + 1] + nums[j];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (leftPreSum[i] == rightPreSum[i])
+                return i;
+        }
+        return -1;
+    }
+}
