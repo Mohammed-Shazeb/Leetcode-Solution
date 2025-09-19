2ce9d4e8a15cf3864707285cc5a737c493dc6fa7
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-09-19 15:13:02 +0000

    [LeetCode Sync] Runtime - 11 ms (14.80%), Memory - 46.3 MB (99.99%)

---
+++ solutions/0056-merge-intervals/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] curr = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+
+            if(curr[1] >= next[0]){
+                curr[1] = Math.max(curr[1],next[1]);
+            }else{
+                result.add(curr);
+                curr = next;
+            }
+        }
+        result.add(curr);
+        return result.toArray(new int[result.size()][]);
+    }
+}
