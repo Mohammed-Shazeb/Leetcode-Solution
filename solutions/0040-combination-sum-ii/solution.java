e0dee7712db33c5ac3d9ac792bfa2d77942baf40
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-10-12 17:16:16 +0000

    [LeetCode Sync] Runtime - 11 ms (16.29%), Memory - 43.1 MB (99.99%)

---
+++ solutions/0040-combination-sum-ii/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    private void backtrack(int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result,
+            int sum) {
+        Arrays.sort(candidates);
+        if (sum == target) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        if (sum > target)
+            return;
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1])continue;
+            current.add(candidates[i]);
+            backtrack(candidates, target, i+1, current, result, sum + candidates[i]);
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result, 0);
+        return result;
+    }
+
+}
