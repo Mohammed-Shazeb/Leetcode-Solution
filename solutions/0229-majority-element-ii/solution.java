567519789dc8b41feab9744d3a6112444ad36eb4
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-11-02 16:07:02 +0000

    [LeetCode Sync] Runtime - 16 ms (21.57%), Memory - 52.5 MB (66.26%)

--- solutions/0229-majority-element-ii/solution.java
+++ solutions/0229-majority-element-ii/solution.java
@@ -1,42 +1,21 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 1;
-        int count1 = 0, count2 = 0;
         List<Integer> list = new ArrayList<>();
-        int n = nums.length;
-
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+            if(map.containsKey(curr)){
+                map.put(curr,map.get(curr)+1);
+            }else{
+                map.put(curr,1);
             }
         }
-        count1 = 0;
-        count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1)
-                count1++;
-            else if (num == candidate2)
-                count2++; 
+        int threshold = nums.length / 3;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > threshold) {
+                list.add(entry.getKey());
+            }
         }
-        if (count1 > n / 3)
-            list.add(candidate1);
-
-        if (count2 > n / 3 && candidate2 != candidate1)
-            list.add(candidate2);
-
         return list;
     }
 }
-
