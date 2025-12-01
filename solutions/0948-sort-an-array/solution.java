c25c1e63fa79f1665ea6154a3ae78112dc28ab02
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-12-01 10:53:45 +0000

    [LeetCode Sync] Runtime - 24 ms (73.00%), Memory - 86.8 MB (11.94%)

---
+++ solutions/0948-sort-an-array/solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int k = 0;
+        int i = si, j = mid + 1;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        // left part
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right part
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // copying temp to original
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+        
+    }
+}
