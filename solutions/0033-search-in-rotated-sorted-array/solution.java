44416a38fe07769804f3c08049ac636ecaca558d
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-12-19 11:38:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.9 MB (27.83%)

--- solutions/0033-search-in-rotated-sorted-array/solution.java
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,33 +1,32 @@
 class Solution {
-    public static int helper(int[] arr, int target, int si, int ei){
-        int mid = si +(ei-si)/2;
-        if(si>ei){
-            return -1;
-        }
-        if(arr[mid] == target){
-            return mid;
-        }
-        // Left side is sorted
-        if (arr[si] <= arr[mid]) {
-            if (arr[si] <= target && target <= arr[mid]) {
-                return helper(arr, target, si, mid - 1);
-            } else {
-                return helper(arr, target, mid + 1, ei);
-            }
-        } 
-        // Right side is sorted
-        else {
-            if (arr[mid] <= target && target <= arr[ei]) {
-                return helper(arr, target, mid + 1, ei);
-            } else {
-                return helper(arr, target, si, mid - 1);
-            }
-        }
-    }
     public int search(int[] nums, int target) {
         int si = 0;
-        int ei = nums.length-1;
-        return helper(nums, target, si,ei);
-        
+        int ei = nums.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[si] <= nums[mid]) {
+                if (nums[si] <= target && target <= nums[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            }
+            // Right side is sorted
+            else {
+                if (nums[mid] <= target && target <= nums[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            }
+
+        }
+        return -1;
     }
 }
