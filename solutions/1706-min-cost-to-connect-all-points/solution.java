caa0959755a5ec2bce8ddcd83fa0e8bef3c5d4b2
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-12-26 08:44:48 +0000

    [LeetCode Sync] Runtime - 592 ms (21.49%), Memory - 76.9 MB (57.01%)

---
+++ solutions/1706-min-cost-to-connect-all-points/solution.java
@@ -0,0 +1,47 @@
+import java.util.PriorityQueue;
+
+class Solution {
+
+    static class Pair implements Comparable<Pair> {
+        int v;
+        int cost;
+
+        public Pair(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.cost - p2.cost;
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        boolean[] vis = new boolean[n];
+        int totalCost = 0;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (vis[curr.v]) continue;
+
+            vis[curr.v] = true;
+            totalCost += curr.cost;
+
+            for (int i = 0; i < n; i++) {
+                if (!vis[i]) {
+                    int dist = Math.abs(points[curr.v][0] - points[i][0]) +
+                               Math.abs(points[curr.v][1] - points[i][1]);
+                    pq.add(new Pair(i, dist));
+                }
+            }
+        }
+
+        return totalCost;
+    }
+}
+
