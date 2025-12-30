6f925f6ef6f8f5d1430e60b7b2c69f872ff5410d
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2025-12-30 11:43:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.5 MB (95.95%)

---
+++ solutions/0733-flood-fill/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void dfs(int[][] image, int r, int c, int originalColor, int newColor) {
+        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length)
+            return;
+
+        if (image[r][c] != originalColor)
+            return;
+
+        image[r][c] = newColor;
+
+        dfs(image, r + 1, c, originalColor, newColor);
+        dfs(image, r - 1, c, originalColor, newColor);
+        dfs(image, r, c + 1, originalColor, newColor);
+        dfs(image, r, c - 1, originalColor, newColor);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if (image[sr][sc] == color)
+            return image;
+
+        dfs(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+}
+
