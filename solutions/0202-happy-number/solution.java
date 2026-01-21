5d83859631e4066695339eeaef0752609c768c4f
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-01-21 10:17:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.6 MB (34.55%)

---
+++ solutions/0202-happy-number/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1 || n==7) return true;
+        while(n>9){
+            int sum = 0;
+            while (n > 0) {
+                int ld = n % 10;
+                sum += ld * ld;
+                n /= 10;
+            }
+            n = sum;
+            if(sum == 1|| sum == 7){
+                return true;
+            }
+        }
+        return false;
+    }
+}
