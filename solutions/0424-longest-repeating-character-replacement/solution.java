da6c1a4df914617604ebb962b97721aa8fee773a
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-01-28 13:53:12 +0000

    [LeetCode Sync] Runtime - 11 ms (50.89%), Memory - 46.3 MB (50.99%)

--- solutions/0424-longest-repeating-character-replacement/solution.java
+++ solutions/0424-longest-repeating-character-replacement/solution.java
@@ -3,24 +3,25 @@ class Solution {
         int start = 0;
         int[] freq = new int[26];
         int maxLen = 0;
-        int maxVal = 0;
+        int maxFreq = 0;
 
-        for (int end = 0; end < s.length(); end++) {
+        for(int end=0; end<s.length(); end++) {
             int idx = s.charAt(end) - 'A';
             freq[idx]++;
 
-            maxVal = Math.max(maxVal, freq[idx]);
+            maxFreq = Math.max(maxFreq, freq[idx]);
 
-            int windowSize = end - start + 1;
+            int windowSize = end-start+1;
 
-            if (windowSize - maxVal > k) {
-                // Shrink the window from the left
+            int changes_needed_to_be_req = windowSize - maxFreq;
+
+            if(changes_needed_to_be_req > k) {
                 freq[s.charAt(start) - 'A']--;
                 start++;
             }
-            // Update maxLen after adjusting window
-            maxLen = Math.max(maxLen, end - start + 1);
+            maxLen = Math.max(maxLen, end-start+1);
+
         }
-        return maxLen;
+        return maxLen;    
     }
 }
