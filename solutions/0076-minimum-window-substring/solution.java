24776e248393232a9e37cb3f20c17f6fd45fd7fa
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-01-29 14:20:27 +0000

    [LeetCode Sync] Runtime - 2 ms (99.77%), Memory - 45.7 MB (66.84%)

--- solutions/0076-minimum-window-substring/solution.java
+++ solutions/0076-minimum-window-substring/solution.java
@@ -1,3 +1,72 @@
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         // Handle empty strings
+//         if (s.isEmpty() || t.isEmpty()) {
+//             return "";
+//         }
+
+//         // Frequency map for characters in t
+//         int[] charCounts = new int[256]; // Using an array for ASCII characters
+
+//         // Populate charCounts with frequencies of characters in t
+//         for (char c : t.toCharArray()) {
+//             charCounts[c]++;
+//         }
+
+//         int left = 0;
+//         int right = 0;
+//         int minLength = Integer.MAX_VALUE;
+//         int startIndex = -1;
+//         int count = 0; // To track how many characters from t are currently in the window
+
+//         // N and M sizes
+//         int n = s.length();
+//         int m = t.length();
+
+//         // Sliding window loop (expand with right pointer)
+//         while (right < n) {
+//             char rChar = s.charAt(right);
+
+//             // If the character is needed from t, decrement its count in map and increment 'count'
+//             // A character is "needed" if its count in charCounts is still positive (meaning we still need to find it)
+//             if (charCounts[rChar] > 0) {
+//                 count++;
+//             }
+//             charCounts[rChar]--; // Decrement the count for the character at right pointer
+
+//             // When all characters from t are found in the current window
+//             while (count == m) {
+//                 // Update minimum length and starting index
+//                 if (right - left + 1 < minLength) {
+//                     minLength = right - left + 1;
+//                     startIndex = left;
+//                 }
+
+//                 // Try to shrink the window from the left
+//                 char lChar = s.charAt(left);
+//                 charCounts[lChar]++; // Increment the count as we remove it from the window
+
+//                 // If removing this character means we now need it again for t,
+//                 // decrement 'count' because the window is no longer "valid" for t
+//                 if (charCounts[lChar] > 0) {
+//                     count--;
+//                 }
+//                 left++; // Move left pointer to shrink the window
+//             }
+//             right++; // Move right pointer to expand the window
+//         }
+
+//         // If no valid window was found
+//         if (startIndex == -1) {
+//             return "";
+//         }
+
+//         // Return the minimum window substring
+//         return s.substring(startIndex, startIndex + minLength);
+
+//     }
+// }
+
 class Solution {
     public String minWindow(String s, String t) {
         // Handle empty strings
@@ -5,64 +74,49 @@ class Solution {
             return "";
         }
 
-        // Frequency map for characters in t
-        int[] charCounts = new int[256]; // Using an array for ASCII characters
+        int charCount[] = new int[256];
 
-        // Populate charCounts with frequencies of characters in t
-        for (char c : t.toCharArray()) {
-            charCounts[c]++;
+        for (int i = 0; i < t.length(); i++) {
+            char curr = t.charAt(i);
+            charCount[curr]++;
         }
 
-        int left = 0;
-        int right = 0;
-        int minLength = Integer.MAX_VALUE;
-        int startIndex = -1;
-        int count = 0; // To track how many characters from t are currently in the window
-
-        // N and M sizes
         int n = s.length();
         int m = t.length();
 
-        // Sliding window loop (expand with right pointer)
+        int left = 0;
+        int right = 0;
+        int minLen = Integer.MAX_VALUE;
+        int si = -1;
+        int count = 0;
+
         while (right < n) {
             char rChar = s.charAt(right);
 
-            // If the character is needed from t, decrement its count in map and increment 'count'
-            // A character is "needed" if its count in charCounts is still positive (meaning we still need to find it)
-            if (charCounts[rChar] > 0) {
+            if (charCount[rChar] > 0) {
                 count++;
             }
-            charCounts[rChar]--; // Decrement the count for the character at right pointer
- 
-            // When all characters from t are found in the current window
+            charCount[rChar]--;
+
             while (count == m) {
-                // Update minimum length and starting index
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    startIndex = left;
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    si = left;
                 }
+                charCount[s.charAt(left)]++;
 
-                // Try to shrink the window from the left
-                char lChar = s.charAt(left);
-                charCounts[lChar]++; // Increment the count as we remove it from the window
-
-                // If removing this character means we now need it again for t,
-                // decrement 'count' because the window is no longer "valid" for t
-                if (charCounts[lChar] > 0) {
+                if (charCount[s.charAt(left)] > 0) {
                     count--;
                 }
-                left++; // Move left pointer to shrink the window
+                left++;
             }
-            right++; // Move right pointer to expand the window
+            right++;
         }
-
-        // If no valid window was found
-        if (startIndex == -1) {
+        if (si == -1) {
             return "";
         }
 
         // Return the minimum window substring
-        return s.substring(startIndex, startIndex + minLength);
-
+        return s.substring(si, si + minLen);
     }
 }
