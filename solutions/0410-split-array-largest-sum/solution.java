5f6f9cf611e6320f72fa18fc31cb6a5c43094c01
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-02-06 17:02:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (33.17%)

--- solutions/0410-split-array-largest-sum/solution.java
+++ solutions/0410-split-array-largest-sum/solution.java
@@ -2,32 +2,35 @@ class Solution {
     private boolean isValid(int nums[], int k, int mid) {
         int subArrSum = 0;
         int split = 1;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] > mid) return false;
-            if(nums[i] + subArrSum <= mid) {
-                subArrSum += nums[i];
+
+        for (int num : nums) {
+            if(num > mid) return false;
+            if (num + subArrSum <= mid) {
+                subArrSum += num;
             } else {
                 split++;
-                subArrSum = nums[i];
+                subArrSum = num;
             }
         }
-        return split>k?false:true;
+
+        return split > k ? false : true;
     }
+
     public int splitArray(int[] nums, int k) {
         int sum = 0;
-        for(int num:nums) {
+        for (int num : nums) {
             sum += num;
         }
         int start = 0;
         int end = sum;
-        int ans = -1;
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(isValid(nums, k, mid)) {
+        int ans = 0;
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (isValid(nums, k, mid)) {
                 ans = mid;
-                end = mid-1;
-            }else{
-                start = mid+1;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
         return ans;
