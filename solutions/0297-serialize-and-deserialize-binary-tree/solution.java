6a83537ca07c243f82fefbd6f8b43f1cb23b42d3
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-03-02 06:14:23 +0000

    [LeetCode Sync] Runtime - 15 ms (48.98%), Memory - 47.7 MB (68.39%)

--- solutions/0297-serialize-and-deserialize-binary-tree/solution.java
+++ solutions/0297-serialize-and-deserialize-binary-tree/solution.java
@@ -13,19 +13,19 @@ public class Codec {
     public String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
 
-        if (root == null)
-            return null;
+        if(root == null) return null;
 
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
 
-        while (!q.isEmpty()) {
-            TreeNode curr = q.remove();
+        while(!q.isEmpty()) {
+            TreeNode curr = q.poll();
 
-            if (curr == null) {
+            if(curr == null) {
                 sb.append("null,");
                 continue;
             }
+
             sb.append(curr.val).append(",");
 
             q.add(curr.left);
@@ -36,36 +36,32 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data == null || data.isEmpty())
-            return null;
+        if(data == null || data.isEmpty()) return null;
 
-        String[] nodes = data.split(",");
+        String nodes[] = data.split(",");
 
         TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
         Queue<TreeNode> q = new LinkedList<>();
+
         q.add(root);
 
-        int i = 1;
-        while (!q.isEmpty() && i < nodes.length) {
+        int i=1;
+        while(!q.isEmpty()) {
             TreeNode curr = q.poll();
-
-            // left child
-            if (!nodes[i].equals("null")) {
+            
+            if(!nodes[i].equals("null")) {
                 curr.left = new TreeNode(Integer.parseInt(nodes[i]));
                 q.add(curr.left);
             }
             i++;
 
-            // right child
-            if (i < nodes.length && !nodes[i].equals("null")) {
+            if(i<nodes.length && !nodes[i].equals("null")) {
                 curr.right = new TreeNode(Integer.parseInt(nodes[i]));
                 q.add(curr.right);
             }
             i++;
         }
-
         return root;
-
     }
 }
 
