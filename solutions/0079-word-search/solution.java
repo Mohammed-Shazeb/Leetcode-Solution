9fd7b09416f7f041de34af1c6b6191cd1b18bfa7
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-03-20 10:28:44 +0000

    [LeetCode Sync] Runtime - 187 ms (19.23%), Memory - 43.1 MB (44.25%)

---
+++ solutions/0079-word-search/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    int rows, cols;
+    int[][] dirs = {
+        {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+    };
+
+    public boolean dfs(char[][] board, String word, int r, int c, int index) {
+        if (index == word.length()) return true;
+
+        // Out of bounds check
+        if (r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[r][c];
+        board[r][c] = '#';           
+
+        // Explore all 4 directions
+        for (int[] d : dirs) {
+            if (dfs(board, word, r + d[0], c + d[1], index + 1)) {
+                return true;
+            }
+        }
+
+        board[r][c] = temp;
+        return false;
+    }
+    public boolean exist(char[][] board, String word) {
+        rows = board.length;
+        cols = board[0].length;
+
+        // Try starting from every cell
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (dfs(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+    
