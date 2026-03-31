9af1e7244510aeb3c1278889a4129780dc07e740
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-03-31 12:09:02 +0000

    [LeetCode Sync] Runtime - 3 ms (39.85%), Memory - 46.4 MB (82.43%)

--- solutions/0051-n-queens/solution.java
+++ solutions/0051-n-queens/solution.java
@@ -7,7 +7,6 @@ class Solution {
                 board[i][j] = '.';
             }
         } 
-
         backtrack(board, 0, solution);
         return solution;
     }
@@ -16,33 +15,29 @@ class Solution {
     public void backtrack(char[][] board, int row, List<List<String>> solutions) {
         if(row == board.length) {
             solutions.add(convertReturnFormat(board));
-        }else {
-            for(int i = 0; i < board.length; i++){
-                if(isSafe(board, row, i)){
-                    board[row][i] = 'Q';
-                    backtrack(board, row+1, solutions);
-                    board[row][i] = '.';
-                }
+        }
+
+        for(int i = 0; i < board.length; i++){
+            if(isSafe(board, row, i)){
+                board[row][i] = 'Q';
+                backtrack(board, row+1, solutions);
+                board[row][i] = '.';
             }
         }
+        
     }
 
     public boolean isSafe(char board[][], int row, int col) {
-        for (int i = row - 1; i >= 0; i--) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
+        for(int i=row-1; i>=0; i--) {
+            if(board[i][col] == 'Q') return false;
         }
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if(board[i][j] == 'Q') {
-                return false;
-            }
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--) {
+            if(board[i][j] == 'Q') return false;
         }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if(board[i][j] == 'Q') {
-                return false;
-            }
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--,j++) {
+            if(board[i][j] == 'Q') return false;
         }
+        
         return true;
     }
 
@@ -56,6 +51,5 @@ class Solution {
 
         return solution;
     }
-
 
 }
