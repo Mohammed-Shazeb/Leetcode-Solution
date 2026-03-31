53a6d8589b62bd6dbcf4720139748054252369c5
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-03-31 12:01:59 +0000

    [LeetCode Sync] Runtime - 2 ms (61.60%), Memory - 42.1 MB (59.18%)

--- solutions/0052-n-queens-ii/solution.java
+++ solutions/0052-n-queens-ii/solution.java
@@ -1,29 +1,33 @@
 class Solution {
-    public boolean isSafe(boolean board[][],int row,int col){
+    int count = 0;
+    public boolean isSafe(boolean board[][],int row,int col) {
         for(int i=row-1;i>=0;i--){
             if(board[i][col] == true) return false;
         }
-        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j-- ){
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j-- ) {
             if(board[i][j] == true) return false;
         }
-        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++ ){
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++ ) {
             if(board[i][j] == true) return false;
         }
         return true;
     }
+    
     public int nQueen(boolean board[][],int row){
         if(row == board.length){
+            count++;
             return 1;
         }
-        int ans = 0;
+        // int ans = 0;
         for(int j=0;j<board.length;j++){
            if(isSafe(board,row,j)){
                 board[row][j] = true;
-                ans += nQueen(board, row+1);
+                nQueen(board, row+1);
                 board[row][j] = false;
            }
         }
-        return ans;
+        // return ans;
+        return count;
     }
     
     public int totalNQueens(int n) {
