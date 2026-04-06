d862fdfc54ca70a629294fbebf7764e9469ac185
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-06 11:41:19 +0000

    [LeetCode Sync] Runtime - 17 ms (66.92%), Memory - 47.2 MB (8.27%)

--- solutions/2755-extra-characters-in-a-string/solution.java
+++ solutions/2755-extra-characters-in-a-string/solution.java
@@ -1,21 +1,50 @@
 class Solution {
-    private int helper(int start, String s, Set<String> dict, int[] dp) {
-        if (start == s.length()) return 0;
+    class TrieNode {
+        TrieNode children[] = new TrieNode[26];
+        boolean eow = false;
+       
+        TrieNode() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
 
-        
+    public TrieNode root = new TrieNode();
+
+    public void insert(String word) {
+        TrieNode curr = root;
+
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    private int helper(int start, String s, Set<String> dict, int dp[]) {
+        if (start == s.length()) return 0;
 
         if (dp[start] != -1) return dp[start];
 
-        int ans = Integer.MAX_VALUE;
+        int ans = 1 + helper(start + 1, s, dict, dp);
 
-        for (int end = start + 1; end <= s.length(); end++) {
-            String word = s.substring(start, end);
+        TrieNode node = root;
 
-            int count = (dict.contains(word) ? 0 : word.length()) + helper(end, s, dict, dp);
+        for (int i = start; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
-            ans = Math.min(ans, count);
+            if (node.children[ch - 'a'] == null) break;
+
+            node = node.children[ch - 'a'];
+
+            if (node.eow) {
+                ans = Math.min(ans, helper(i + 1, s, dict, dp));
+            }
         }
-        
         dp[start] = ans;
         return ans;
     }
@@ -24,6 +53,8 @@ class Solution {
         Set<String> dict = new HashSet<>(Arrays.asList(dictionary));
         int[] dp = new int[s.length() + 1];
         Arrays.fill(dp, -1);
+
+        for (String word : dictionary) insert(word);
         return helper(0, s, dict, dp);
     }
 }
