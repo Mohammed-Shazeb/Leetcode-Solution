89bf98276b5434dbaef37da97f9b0430c91005e1
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-10 15:48:47 +0000

    [LeetCode Sync] Runtime - 4 ms (99.82%), Memory - 47.5 MB (24.66%)

--- solutions/0463-island-perimeter/solution.java
+++ solutions/0463-island-perimeter/solution.java
@@ -4,13 +4,11 @@ class Solution {
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    perimeter += 4;
-                    if (i > 0 && grid[i - 1][j] == 1)
-                        perimeter -= 2;
-                    if (j > 0 && grid[i][j - 1] == 1)
-                        perimeter -= 2;
-                }
+                if(grid[i][j] == 1) {
+                    perimeter+=4;
+                    if(i>0 && grid[i-1][j] == 1) perimeter-=2;
+                    if(j>0 && grid[i][j-1] == 1) perimeter-=2;
+                } 
             }
         }
         return perimeter;
