e6e8aae4450e05529c1b013b1b6f461f4287da08
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-11 14:39:21 +0000

    [LeetCode Sync] Runtime - 1 ms (52.33%), Memory - 43.9 MB (15.62%)

--- solutions/0990-verifying-an-alien-dictionary/solution.java
+++ solutions/0990-verifying-an-alien-dictionary/solution.java
@@ -6,31 +6,26 @@ class Solution {
             map.put(order.charAt(i), i);
         }
 
-        // Compare each adjacent pair of words
-        for (int i = 0; i < words.length - 1; i++) {
+        for(int i=0; i<words.length-1; i++) {
             String s1 = words[i];
             String s2 = words[i + 1];
-            
-            boolean valid = false; // flag to track if a difference was found
+
+            boolean valid = false;
             int n = Math.min(s1.length(), s2.length());
 
-            for (int j = 0; j < n; j++) {
+            for(int j=0; j<n; j++) {
                 int rank1 = map.get(s1.charAt(j));
                 int rank2 = map.get(s2.charAt(j));
 
-                if (rank1 < rank2) {
-                    valid = true; // correct order
+                if(rank1<rank2) {
+                    valid = true;
                     break;
-                } else if (rank1 > rank2) {
-                    return false; // wrong order
+                } else if(rank1>rank2) {
+                    return false;
                 }
-                // else rank1 == rank2, continue to next character
             }
-
-            // If all characters are same up to min length, check lengths
             if (!valid && s1.length() > s2.length()) return false;
         }
-
         return true;
     }
 }   
