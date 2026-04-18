4280dbbc83bf42cd266fafe5ffeae16b8d39079f
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-18 10:34:58 +0000

    [LeetCode Sync] Runtime - 2 ms (96.47%), Memory - 45.6 MB (68.53%)

--- solutions/0039-combination-sum/solution.java
+++ solutions/0039-combination-sum/solution.java
@@ -1,23 +1,24 @@
 class Solution {
-    private void backtrack(int[] candidates, int target, int start, List<Integer> current, List<List<Integer>> result,
-            int sum) {
-        if (sum == target) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-        if (sum > target) {
-            return;
-        }
-        for (int i = start; i < candidates.length; i++) {
-            current.add(candidates[i]);
-            backtrack(candidates, target, i, current, result, sum + candidates[i]);
-            current.remove(current.size() - 1);
-        }
-    }
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates, target, 0, new ArrayList<>(), result, 0);
+        List<Integer> accumulator = new ArrayList<>();
+        backtrack(0, 0, target, candidates, accumulator, result);
         return result;
+    }
+
+    private void backtrack(int idx, int sum, int target, int[] candidates, List<Integer> accumulator,
+            List<List<Integer>> result) {
+        if (sum == target) {
+            result.add(new ArrayList<>(accumulator));
+            return;
+        }
+        if (sum > target || idx == candidates.length) {
+            return;
+        }
+        int num = candidates[idx];
+        accumulator.add(num);
+        backtrack(idx, sum + num, target, candidates, accumulator, result);
+        accumulator.remove(accumulator.size() - 1);
+        backtrack(idx + 1, sum, target, candidates, accumulator, result);
     }
 }
