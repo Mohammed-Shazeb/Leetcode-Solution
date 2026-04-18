cd7a5f7b593e73d1e129aa95b6e81b563417395f
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-18 10:27:19 +0000

    [LeetCode Sync] Runtime - 1 ms (86.70%), Memory - 44.4 MB (20.51%)

--- solutions/0078-subsets/solution.java
+++ solutions/0078-subsets/solution.java
@@ -1,26 +1,22 @@
-import java.util.*;
 class Solution {
-    public static void findSubsets(int[] nums, List<List<Integer>> result, List<Integer> current, int i) {
-        // Base case: reached end of array
+    public void backtrack(int nums[], List<List<Integer>> res, List<Integer> curr, int i) {
+       
         if (i == nums.length) {
-            result.add(new ArrayList<>(current)); // add a copy of current subset
+            res.add(new ArrayList<>(curr));
             return;
         }
 
-        // Include nums[i] in the current subset
-        current.add(nums[i]);
-        findSubsets(nums, result, current, i + 1);
+        curr.add(nums[i]);
+       
+        backtrack(nums, res, curr, i + 1);
+        curr.remove(curr.size() - 1);
+        backtrack(nums, res, curr, i + 1);
 
-        // Exclude nums[i] from the current subset
-        current.remove(current.size() - 1);
-        findSubsets(nums, result, current, i + 1);
     }
 
-    public static List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        findSubsets(nums, result, new ArrayList<>(), 0);
+        backtrack(nums, result, new ArrayList<>(), 0);
         return result;
     }
-
 }
-
