a350403fb87b9fa5f7623434c3020a674bfa1855
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-23 06:49:56 +0000

    [LeetCode Sync] Runtime - 91 ms (34.70%), Memory - 50.3 MB (10.24%)

---
+++ solutions/0127-word-ladder/solution.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+
+        for (String word : wordList) {
+            wordSet.add(word); 
+        }
+        Queue<String> q = new LinkedList<>();
+        q.add(beginWord);
+
+        visited.add(beginWord);
+
+        int level = 1;
+
+        while(!q.isEmpty()) {
+            int n = q.size();
+
+            while(n-- > 0) {
+                String curr = q.poll();
+
+                if (curr.equals(endWord)) return level;
+
+                StringBuilder sb = new StringBuilder(curr);
+
+                for (int i = 0; i < sb.length(); i++) {
+                    char original = sb.charAt(i);
+
+                    for (char ch = 'a'; ch<='z'; ch++) {
+                        if (ch == original) continue;
+
+                        sb.setCharAt(i, ch);
+                        String neighbour = sb.toString();
+
+                        if (wordSet.contains(neighbour) && !visited.contains(neighbour)) {
+                            visited.add(neighbour);
+                            q.add(neighbour);
+                        }
+                    }
+                    sb.setCharAt(i, original);
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
