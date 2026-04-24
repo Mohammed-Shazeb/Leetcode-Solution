3e99a990e0d609c12336c2a9f17f89c6cf7b5098
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-24 11:24:00 +0000

    [LeetCode Sync] Runtime - 2 ms (99.04%), Memory - 46.2 MB (94.89%)

--- solutions/0572-subtree-of-another-tree/solution.java
+++ solutions/0572-subtree-of-another-tree/solution.java
@@ -14,28 +14,21 @@
  * }
  */
 class Solution {
-    public boolean isIdentical(TreeNode node, TreeNode subRoot) {
-        if (node == null && subRoot == null) {
-            return true;
-        } else if (node == null || subRoot == null || node.val != subRoot.val) {
-            return false;
-        }
-        if (!isIdentical(node.left, subRoot.left)) {
-            return false;
-        }
-        if (!isIdentical(node.right, subRoot.right)) {
-            return false;
-        }
+    public boolean isSame(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true;
+        if(root == null || subRoot == null || root.val != subRoot.val) return false;
+        if(!isSame(root.left, subRoot.left)) return false;
+        if(!isSame(root.right, subRoot.right)) return false;
+
         return true;
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) {
-            return false;
+        if (root == null) return false;
+
+        if(root.val == subRoot.val) {
+            if(isSame(root, subRoot)) return true;
         }
-        if (root.val == subRoot.val) {
-            if (isIdentical(root, subRoot))
-                return true;
-        }
+
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 }
