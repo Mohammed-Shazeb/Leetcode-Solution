421331e5d1227aa66ff53c859ba0c3dc8e74293e
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-04-29 16:33:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (50.48%)

--- solutions/0104-maximum-depth-of-binary-tree/solution.java
+++ solutions/0104-maximum-depth-of-binary-tree/solution.java
@@ -17,8 +17,9 @@ class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
 
-        int lh = maxDepth(root.left);
-        int rh = maxDepth(root.right);
-        return Math.max(lh, rh) + 1;
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
