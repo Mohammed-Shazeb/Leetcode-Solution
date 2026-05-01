c93540cbe3b7e31167f9520b15db234ec880fcbd
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-05-01 11:54:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47 MB (50.25%)

--- solutions/0784-insert-into-a-binary-search-tree/solution.java
+++ solutions/0784-insert-into-a-binary-search-tree/solution.java
@@ -15,17 +15,16 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null){
+        if(root == null) { 
             root = new TreeNode(val);
             return root;
-        }
-
-        if(root.val > val) {
+        }           
+        if(val < root.val) {
             root.left = insertIntoBST(root.left, val);
-        } else {
+        }
+        if(val > root.val) {
             root.right = insertIntoBST(root.right, val);
         }
-
         return root;
     }
 }
