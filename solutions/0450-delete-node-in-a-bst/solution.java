208542808fa33daf4d0e18349c59c22f9157cb86
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-05-03 03:59:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47.6 MB (37.91%)

--- solutions/0450-delete-node-in-a-bst/solution.java
+++ solutions/0450-delete-node-in-a-bst/solution.java
@@ -14,38 +14,38 @@
  * }
  */
 class Solution {
-    public TreeNode fIS(TreeNode root){
-        while(root.left!=null){
+    public TreeNode fIS(TreeNode root) {
+        while (root.left != null) {
             root = root.left;
         }
         return root;
     }
+
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null){
+        if (root == null)
             return null;
-        }
-        if(root.val < key){
+
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
             root.right = deleteNode(root.right, key);
         }
-        else if(root.val > key){
-            root.left = deleteNode(root.left, key);
-        }
-        else{
-            if(root.left == null && root.right == null){
+
+        else {
+            if (root.left == null && root.right == null)
                 return null;
-            }
-            else if(root.left == null ){
+
+            else if (root.left == null)
                 return root.right;
-            }
-            else if(root.right == null){
+            else if (root.right == null)
                 return root.left;
-            }
-            else{
+
+            else {
                 TreeNode ls = fIS(root.right);
                 root.val = ls.val;
                 root.right = deleteNode(root.right, ls.val);
             }
         }
-        return root; 
+        return root;
     }
 }
