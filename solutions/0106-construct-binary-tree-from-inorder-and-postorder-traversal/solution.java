6faf22b7b6191ad773f3471c77c861e5e9d2b90d
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-05-09 07:39:14 +0000

    [LeetCode Sync] Runtime - 1 ms (98.88%), Memory - 46.2 MB (65.64%)

--- solutions/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.java
+++ solutions/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.java
@@ -23,20 +23,18 @@ class Solution {
         return buildTree(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1, inOrderMap);
     }
 
-    private TreeNode buildTree(int[] postorder, int postStart, int postEnd,
-                        int[] inorder, int inStart, int inEnd, 
-                        Map<Integer, Integer> inOrderMap) {
-        if (postStart > postEnd || inStart > inEnd)
-            return null;
+        private TreeNode buildTree(int[] postorder, int postStart, int postEnd, int[] inorder, 
+                                int inStart, int inEnd, Map<Integer, Integer> inOrderMap) {
+        if(postStart > postEnd || inStart > inEnd) return null;
 
         TreeNode root = new TreeNode(postorder[postEnd]);
 
-        int inRoot = inOrderMap.get(root.val); // get the root index in O(1) 
-        int numsLeft = inRoot - inStart; // this gives number of left nodes in the tree from the inorder[]
+        int inRoot = inOrderMap.get(root.val);
+        int numsLeft = inRoot - inStart;
 
         root.left = buildTree(postorder, postStart, postStart + numsLeft - 1, inorder, inStart, inRoot - 1, inOrderMap);
         root.right = buildTree(postorder, postStart + numsLeft, postEnd - 1, inorder, inRoot + 1, inEnd, inOrderMap);
-
+    
         return root;
     }
 }
