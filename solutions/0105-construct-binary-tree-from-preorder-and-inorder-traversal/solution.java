d8ad902283000f45a41185a8f81d30179898cc15
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-05-09 07:51:05 +0000

    [LeetCode Sync] Runtime - 1 ms (97.34%), Memory - 46 MB (74.92%)

--- solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
+++ solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
@@ -15,27 +15,24 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> inOrderMap = new HashMap<>();
-
+       Map<Integer, Integer> inOrderMap = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             inOrderMap.put(inorder[i], i);
         }
-
-        TreeNode root = buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inOrderMap);
-        return root;
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inOrderMap); 
     }
-
-    public TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd,
-            Map<Integer, Integer> inOrderMap) {
-        if (preStart > preEnd || inStart > inEnd)
-            return null;
+    
+    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, 
+                                int inStart, int inEnd, Map<Integer, Integer> inOrderMap) {
+        
+        if(preStart > preEnd || inStart > inEnd) return null;
 
         TreeNode root = new TreeNode(preorder[preStart]);
 
-        int inRoot = inOrderMap.get(root.val); // get the root index in O(1) 
-        int numsLeft = inRoot - inStart; // this gives number of left nodes in the tree from the inorder[]
+        int inRoot = inOrderMap.get(root.val);
+        int numsLeft = inRoot - inStart;
 
-        root.left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inOrderMap);
+        root.left = buildTree(preorder, preStart + 1, preStart + numsLeft , inorder, inStart, inRoot - 1, inOrderMap);
         root.right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inOrderMap);
 
         return root;
