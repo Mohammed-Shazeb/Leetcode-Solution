8babcdea46a9081222fcb935dd97358af8b70a0d
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-05-09 06:37:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.7 MB (95.95%)

--- solutions/0098-validate-binary-search-tree/solution.java
+++ solutions/0098-validate-binary-search-tree/solution.java
@@ -14,15 +14,13 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return helper(root, null, null);
-    }
-    private boolean helper(TreeNode root, TreeNode min, TreeNode max) {
-        if(root == null) return true;
-
-        if(min != null && root.val <= min.val) return false;
-        if(max != null && root.val >= max.val) return false;
-
+    public static boolean helper(TreeNode root, TreeNode min,TreeNode max){
+        if(root == null)return true;
+        if((min != null && root.val <= min.val) || (max != null && root.val >= max.val)) return false;        
+        
         return helper(root.left,min,root) && helper(root.right,root,max);
+    }
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,null,null);
     }
 }
