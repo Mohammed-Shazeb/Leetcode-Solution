669d6a55dcb5d55dbbb2b04c06a4b9fd872685e7
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-05-17 05:55:01 +0000

    [LeetCode Sync] Runtime - 6 ms (71.92%), Memory - 47.2 MB (49.38%)

--- solutions/0207-course-schedule/solution.java
+++ solutions/0207-course-schedule/solution.java
@@ -1,64 +1,49 @@
 class Solution {
-    static class Edge {
-        int src;
-        int dest;
-
-        Edge(int src, int dest) {
-            this.src = src;
-            this.dest = dest;
-        }
-    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<Edge> graph[] = new ArrayList[numCourses];
+        ArrayList<Integer> graph[] = new ArrayList[numCourses];
 
         // create graph
         for (int i=0; i<numCourses; i++){
             graph[i] = new ArrayList<>();
         }
 
-        // add the src and dest in the graph
+        int indeg[] = new int[graph.length];
+
         for (int[] p : prerequisites) {
             int src = p[1];
             int dest = p[0];
-            graph[src].add(new Edge(src, dest));
+            graph[src].add(dest);
+            indeg[dest]++;
         }
 
-        // made the in-degree arr and add the incoming node count 
-        // indeg[i] = number of courses that must be done BEFORE course i
-        int indeg[] = new int[graph.length];
-        for(int i=0; i<numCourses; i++){
-            for(Edge e : graph[i]){
-                indeg[e.dest]++;
-            }
-        }
-
-        // make a queue and add the in-degree for each node  
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<indeg.length; i++) {
             if(indeg[i] == 0) {
                 q.add(i);
             }
         }
-        int res[] = new int[numCourses];
+        
         int idx = 0;
 
-        // remove from the queue and add in the res array and 
-        // reduce the count of the in-degree count and if count of indeg is 0 add in the queue 
         while(!q.isEmpty()){
             int curr = q.remove();
-            res[idx++] = curr;
-
-            for(int i=0;i<graph[curr].size();i++){
-                Edge e = graph[curr].get(i);
-                indeg[e.dest]--;
-                if(indeg[e.dest] == 0){
-                    q.add(e.dest);
+            idx++;
+            
+            // for(int i=0;i<graph[curr].size();i++){
+            //     int e = graph[curr].get(i);
+            //     indeg[e]--;
+            //     if(indeg[e] == 0){
+            //         q.add(e);
+            //     }
+            // }
+            for(int neighbour : graph[curr]){
+                indeg[neighbour]--;
+                if(indeg[neighbour] == 0){
+                    q.add(neighbour);
                 }
             }
         }
-        // return the res arr  
         if (idx == numCourses) return true;
-        // return an empty arr if the res is not true;  
         return false;
     }
 }
