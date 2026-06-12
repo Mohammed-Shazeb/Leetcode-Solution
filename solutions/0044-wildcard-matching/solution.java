faebf6f9428819bdc1d6bf8ab06de37bfdda9fe8
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-06-12 13:47:36 +0000

    [LeetCode Sync] Runtime - 57 ms (16.30%), Memory - 47.5 MB (58.07%)

---
+++ solutions/0044-wildcard-matching/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        boolean dp[][] = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+        for (int j = 1; j < m + 1; j++) {
+            if(p.charAt(j-1) == '*') {
+                dp[0][j] = dp[0][j-1]; 
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if(p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];    // we are ignoring the * and in second we are taking *   
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
