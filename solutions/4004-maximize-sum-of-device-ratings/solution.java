89526dfe2a32a7e58cc0e2a5515b44f98eff628b
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-06-14 15:01:13 +0000

    [LeetCode Sync] Runtime - 42 ms (37.17%), Memory - 210.3 MB (39.78%)

---
+++ solutions/4004-maximize-sum-of-device-ratings/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long maxRatings(int[][] units) {
+        long sumSecond = 0;
+        int minFirst = Integer.MAX_VALUE;
+        int minSecond = Integer.MAX_VALUE;
+
+        for (int[] row : units) {
+            Arrays.sort(row);
+
+            minFirst = Math.min(minFirst, row[0]);
+
+            if (row.length == 1) {
+                sumSecond += row[0];
+                continue;
+            }
+
+            minSecond = Math.min(minSecond, row[1]);
+            sumSecond += row[1];
+        }
+
+        if (minSecond == Integer.MAX_VALUE) {
+            return sumSecond;
+        }
+
+        return sumSecond - minSecond + minFirst;
+
+    }
+}
