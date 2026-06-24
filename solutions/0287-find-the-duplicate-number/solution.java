51cbece2a786a2cfeecf65459ad100456178c36a
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-06-24 11:16:34 +0000

    [LeetCode Sync] Runtime - 5 ms (61.57%), Memory - 82.3 MB (82.45%)

--- solutions/0287-find-the-duplicate-number/solution.java
+++ solutions/0287-find-the-duplicate-number/solution.java
@@ -2,18 +2,20 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int slow = 0;
         int fast = 0;
-        while(true){
+
+        while(true) {
             slow = nums[slow];
             fast = nums[nums[fast]];
+
             if(slow == fast) break;
         }
         slow = 0;
-        while(true){
+
+        while(true) {
             slow = nums[slow];
             fast = nums[fast];
-            if(slow == fast){
-                break;
-            }
+
+            if(slow == fast) break;
         }
 
         return slow;
