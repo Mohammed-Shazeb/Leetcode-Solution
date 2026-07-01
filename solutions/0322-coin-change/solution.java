8102ef776dc802d9e82be0ad4136b80f3f2d6f3a
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-07-01 11:17:52 +0000

    [LeetCode Sync] Runtime - 17 ms (48.29%), Memory - 47.1 MB (20.05%)

--- solutions/0322-coin-change/solution.java
+++ solutions/0322-coin-change/solution.java
@@ -1,16 +1,35 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
+        int n = coins.length;
 
-        Arrays.fill(dp, amount + 1); // acts like infinity
-        dp[0] = 0;
+        int dp[][] = new int[n + 1][amount + 1];
 
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] = Math.min(dp[j], 1 + dp[j - coin]);
-            }
+        // for (int i = 0; i < dp.length; i++) {
+        //     dp[i][0] = 0;
+        // }
+
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = Integer.MAX_VALUE - 1;
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        for (int j = 1; j < dp[0].length; j++) {
+            if(j%coins[0] == 0) {
+                dp[1][j] = 1;
+            } else {
+                dp[1][j] = Integer.MAX_VALUE - 1;
+            }
+        }
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<amount+1; j++) {
+
+                int v = coins[i - 1];
+                if (v <= j) {
+                    dp[i][j] = Math.min(dp[i][j - v] + 1, dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
     }
 }
