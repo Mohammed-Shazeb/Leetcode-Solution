9b873dfa5e95bd8bd613827f5757082683d9cfb6
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-07-02 08:03:16 +0000

    [LeetCode Sync] Runtime - 2 ms (87.60%), Memory - 43.1 MB (88.55%)

--- solutions/0139-word-break/solution.java
+++ solutions/0139-word-break/solution.java
@@ -2,27 +2,35 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> dict = new HashSet<>(wordDict);
         Boolean[] dp = new Boolean[s.length() + 1];
-       return helper(0, s, dict, dp);
+        return helper(0, s, dict, dp);
     }
 
     private boolean helper(int start, String s, Set<String> dict, Boolean[] dp) {
-        
-        if (start == s.length()) return true;
 
-        // Memo check
-        if (dp[start] != null) return dp[start];
+        if (start == s.length())
+            return true;
 
-        for (int end = start + 1; end <= s.length(); end++) {
-            String word = s.substring(start, end);
+        if (dp[start] != null)
+            return dp[start];
 
-            if (dict.contains(word) && helper(end, s, dict, dp)) {
-                dp[start] = true;
-                return true;
+        for (String word : dict) {
+            if (s.startsWith(word, start)) {
+                if (helper(start + word.length(), s, dict, dp)) {
+                    dp[start] = true;
+                    return true;
+                }
             }
         }
 
+        // for(int end = start +1 ; end<=s.length(); end++) {
+        //     String word = s.substring(start, end);
+
+        //     if(dict.contains(word) && helper(end, s, dict, dp)) {
+        //         dp[start] = true;
+        //         return true;
+        //     }
+        // }   
         dp[start] = false;
         return false;
-    
     }
 }
