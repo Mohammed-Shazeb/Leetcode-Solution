cf2287d6f7d48b418a1946d89af359f136087599
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-07-02 10:13:09 +0000

    [LeetCode Sync] Runtime - 3 ms (55.20%), Memory - 49 MB (12.52%)

--- solutions/0140-word-break-ii/solution.java
+++ solutions/0140-word-break-ii/solution.java
@@ -8,18 +8,22 @@ class Solution {
 
     private List<String> helper(String s, Set<String> dict) {
         if (memo.containsKey(s)) return memo.get(s);
+        
         List<String> res = new ArrayList<>();
+
         if (dict.contains(s)) res.add(s);
 
-        for (int i = 1; i < s.length(); i++) {
-            String left = s.substring(0, i);
-            if (!dict.contains(left)) continue;
+        for(int i=1; i<s.length(); i++) {
+            String left = s.substring(0,i);
+
+            if(!dict.contains(left)) continue;
+
             String right = s.substring(i);
-            for (String sub : helper(right, dict)) {
-                res.add(left + " " + sub);
+            for(String sub : helper(right, dict)) {
+                res.add(left+ " "+ sub);
             }
         }
-
+        
         memo.put(s, res);
         return res;
     }
