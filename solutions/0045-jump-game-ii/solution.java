6c820dec0291ee5bb8e6196268f1189985fb62f4
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-07-20 13:09:30 +0000

    [LeetCode Sync] Runtime - 1 ms (99.73%), Memory - 47.1 MB (88.61%)

--- solutions/0045-jump-game-ii/solution.java
+++ solutions/0045-jump-game-ii/solution.java
@@ -1,22 +1,36 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
 
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        dp[n - 1] = 0;
+        // int dp[] = new int[n];
+        // Arrays.fill(dp, -1);
+        // dp[n - 1] = 0;
 
-        for (int i = n - 2; i >= 0; i--) {
-            int steps = nums[i];
-            int ans = Integer.MAX_VALUE;
-            for (int j = i + 1; j <= i + steps && j < n; j++) {
-                if (dp[j] != -1) {
-                    ans = Math.min(ans, dp[j] + 1);
-                }
+        // for (int i = n - 2; i >= 0; i--) {
+        //     int steps = nums[i];
+        //     int ans = Integer.MAX_VALUE;
+        //     for (int j = i + 1; j <= i + steps && j < n; j++) {
+        //         if (dp[j] != -1) {
+        //             ans = Math.min(ans, dp[j] + 1);
+        //         }
+        //     }
+        //     if (ans != Integer.MAX_VALUE)
+        //         dp[i] = ans;
+        // }
+        // return dp[0];
+
+        int res = 0;
+        int l=0,r=0;
+
+        while(r<nums.length-1) {
+            int farthest = 0;
+            for(int i=l;i<=r;i++) {
+                farthest = Math.max(farthest, i+nums[i]);
             }
-            if (ans != Integer.MAX_VALUE)
-                dp[i] = ans;
+            l = r+1;
+            r = farthest;
+            res +=1;
         }
-        return dp[0];
+        return res;
     }
 }
