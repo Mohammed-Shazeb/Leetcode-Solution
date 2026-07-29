1c6881e4f147bca7562fcb58c45cd7cc304ff36b
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-07-29 16:08:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (11.08%)

---
+++ solutions/0678-valid-parenthesis-string/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int open = 0;
+        int close = 0;
+
+        for (char c : s.toCharArray()) {
+            if(c == '(' || c == '*') open++;
+            else open--;
+            if(open < 0) return false;
+        }
+        for (int i = s.length()-1;i>=0;i--) {
+            char c = s.charAt(i);
+            if(c == ')' || c == '*') close++;
+            else close--;
+            if(close < 0) return false;
+        }
+        return true;
+    }
+}
