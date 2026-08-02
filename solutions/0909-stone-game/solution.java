1c123e637d1ea04095110d45aed8ed12e770ee64
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-08-02 05:36:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (41.90%)

--- solutions/0909-stone-game/solution.java
+++ solutions/0909-stone-game/solution.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        // Arrays.sort(piles);
-
-        // int alice= 0;
-        // int bob = 0;
-
-        // for (int i = piles.length - 1; i >= 0; i -= 2) {
-        //     alice += piles[i];
-        // }
-
-        // for (int i = piles.length - 2; i >= 0; i -= 2) {
-        //     bob += piles[i];
-        // }
-        // return alice > bob ? true : false;
         return true;
     }
 }
