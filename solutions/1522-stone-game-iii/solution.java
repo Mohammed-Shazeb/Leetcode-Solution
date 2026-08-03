61cbcb176df7f8527c92448bed2b6cf6b8db62f8
Mohammed Shazeb Shekh <mohammedshazeb.16@gmail.com>
2026-08-03 14:06:07 +0000

    [LeetCode Sync] Runtime - 68 ms (5.10%), Memory - 104.5 MB (5.57%)

--- solutions/1522-stone-game-iii/solution.java
+++ solutions/1522-stone-game-iii/solution.java
@@ -2,7 +2,7 @@ class Solution {
     int n;
     Integer dp[][];
     public int solve(int stoneValue[], int person, int i) {
-        // alice =1 bob =1
+        // alice =1 bob =0
 
         if(i>=n) return 0;
 
@@ -13,26 +13,25 @@ class Solution {
 
         for(int x=1; x<=3 && i+x<=n; x++) {
             stones += stoneValue[i+x-1];
-            if(person == 1) {       // alice
-                result = Math.max(result, stones + solve(stoneValue, 0, i+x));
-            }
-            else {       // bob
-                result = Math.min(result, solve(stoneValue, 1, i+x));
+            if(person==1) {
+                result = Math.max(result, stones+solve(stoneValue, 0,i+x));
+            } else {
+                result = Math.min(result, solve(stoneValue, 1,i+x));
             }
         }
-        dp[person][i] = result;
-        return result;
+        return dp[person][i] = result;
 
     }
     public String stoneGameIII(int[] stoneValue) {
         n = stoneValue.length;
         dp = new Integer[2][50001];
-        int totalScore = 0;
-
-        for(int num:stoneValue) totalScore += num;
+        int total = 0;
+        for(int n:stoneValue) {
+            total+=n;
+        }
         int aliceScore = solve(stoneValue, 1, 0);
+        int diff = 2*aliceScore-total;
         
-        int diff = 2*aliceScore - totalScore;
 
         if(diff >0) return "Alice";
         else if(diff == 0) return "Tie";
